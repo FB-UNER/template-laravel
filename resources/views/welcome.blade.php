c21cc7dde347751ce204825cdf02b79607ffcacf
javparra <javparra@gmail.com>
2017-05-05 10:16:19 -0300

    welcome php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,6 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                     <a href="http://www.fb.uner.edu.ar">Facultad de Bromatología - UNER</a>
                 </div>
             </div>
